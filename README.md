fc1a37f9281787526389dda557cf6bbf3c7b642f
XCKjacksparrow <jackparrow@smail.hyit.edu.cn>
2019-12-10 16:34:29 +0800

    readme.md

---
+++ README.md
@@ -0,0 +1,35 @@
+#Student
+##学生成绩管理系统
+Student是由Springboot2.x + Jpa + Thymeleaf等技术实现的管理网站，如果觉得项目不错，欢迎Star
+
+###适用对象
+1.Spring Boot 初学者。该系统综合运用了作者 kazemi.cn 博客中的的文章提及的知识内容，初学者可以阅读文章以及结合该项目学习。
+2.懵懂者。初次接触Springboot的人。
+
+###技术栈
+####后端
+核心框架：SpringBoot
+持久层框架：Jpa Mybatis
+模板框架：Thymeleaf
+
+####前端
+JS框架：Jquery
+CSS框架：Bootstrap
+
+###预览效果
+####
+![](src/main/resources/static/img/index.png)
+![](src/main/resources/static/img/main.png)
+![](src/main/resources/static/img/add.png)
+
+####安装
+下载源码，执行sql文件，然后修改application-dev.yml文件中连接数据库的用户名、密码。运行项目即可。
+sql文件地址：src/resources/test.sql
+
+访问地址：http://localhost:8888
+
+账户：admin
+密码：123456
+
+####更新日志
+2019-12-12发布第一个版本
---
+++ src/main/resources/test.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: chapter3
+-- ------------------------------------------------------
+-- Server version	5.7.28-0ubuntu0.18.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administrator`
+--
+
+DROP TABLE IF EXISTS `administrator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `administrator` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administrator`
+--
+
+LOCK TABLES `administrator` WRITE;
+/*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
+INSERT INTO `administrator` VALUES (1,'admin','123456');
+/*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `user_name` varchar(20) DEFAULT NULL,
+  `score` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (2,'Jacob','Thornton','@fat',80),(4,'chongkang','xia','@kazemi',99),(5,'zhichao','kong','@qinliwazi',90),(6,'jiabao','wu','@wjb',90),(7,'xun','zhang','@zx',89),(8,'shao','zhang','@zs',80),(9,'chengxiang','cheng','@ccx',70),(10,'jing','xu','@xj',90),(11,'ying','ding','@dy',90),(12,'chongkang','xia','kazm',100),(13,'zhichao','kong','qlwz',90),(14,'CBA','SIKAHA','BBA',99);
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-10 16:09:51
